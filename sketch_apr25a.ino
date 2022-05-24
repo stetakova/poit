21ba6aad186a924955854566a25861a535989992
stetakova <58446616+stetakova@users.noreply.github.com>
2022-05-24 12:48:35 +0200

    Add files via upload
    arduino code

---
+++ sketch_apr25a.ino
@@ -0,0 +1,34 @@
+#define vystup 3
+float value = 5;
+int setting = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(vystup, OUTPUT);
+ 
+}
+
+void loop(){
+
+  float voltage1, voltage2, value;
+  int low=1;
+  
+  voltage1 = (float)analogRead(A2)*5/1023;
+  voltage2 = (float)analogRead(A4)*5/1023;
+  float rozdiel=voltage1-voltage2;
+
+  delay(500);
+  if(rozdiel<0.02){
+  digitalWrite(vystup, LOW);
+  int low=1;
+  }
+  if(rozdiel == 0.00 && low==1){
+    digitalWrite(vystup, HIGH);
+  }
+  delay(500);
+  Serial.println(rozdiel);
+
+  
+ 
+}
